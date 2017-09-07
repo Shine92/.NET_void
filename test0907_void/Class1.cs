77b461fa0d3b9b49458dc51abae0c05271bf0067
shaoming Huang <msn8971@gmail.com>
2017-09-07 14:33:14 +0800

    C#.NET_void
    int =   傳值
    ref =   傳址
    int[] = 傳址

---
+++ test0907_void/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test0907_void {
+
+
+    public class test {
+        public void function1(int a) {
+            a += 101;
+        }
+        public void function2(ref int a) {
+            a += 202;
+        }
+    }
+    class Class1 {
+
+        static void Main(string[] args) {
+            int a = 0;
+            test CTest = new test();
+            CTest.function2(ref a);
+            CTest.function1(a);
+            MessageBox.Show(a.ToString());
+        }
+    }
+}
